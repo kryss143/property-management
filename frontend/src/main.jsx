13dd79f6ca073617dff0b99ac05c6604c9079375
Rod Kristian Rebarbas <kristianrebz@gmail.com>
2025-09-03 20:43:48 +0800

    Added dashboard boilerplate and login boilerplate

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Sidebar from './components/layout/Sidebar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Sidebar />
-  </StrictMode>,
+  </StrictMode>
 )
