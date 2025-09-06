cf36ec48c162152a0be359a9ff1e3c23a1165e8a
Rod Kristian Rebarbas <50227721+kryss143@users.noreply.github.com>
2025-09-06 09:29:05 +0800

    Added calendar
    Added calendar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,5 +5,12 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <>
     <App />
+<<<<<<< Updated upstream
+=======
+    <Sidebar />
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
   </>
 )
