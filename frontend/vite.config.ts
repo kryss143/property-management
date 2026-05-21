ce910bdf2c80b901ac4f619fda2b071e0b549b4e
Rod Kristian Rebarbas <50227721+kryss143@users.noreply.github.com>
2026-05-21 21:12:45 +0800

    to deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,27 +12,27 @@ export default defineConfig({
   },
   build: {
     // Increase warning threshold slightly and add deterministic per-package chunking
-    // chunkSizeWarningLimit: 700,
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks(id) {
-    //       if (!id || !id.includes("node_modules")) return;
-    //       try {
-    //         const parts = id.split("node_modules/")[1].split("/");
-    //         let pkg = parts[0];
-    //         if (pkg.startsWith("@") && parts.length > 1)
-    //           pkg = `${pkg}/${parts[1]}`;
-    //         const safeName = pkg
-    //           .replace("@", "")
-    //           .replace("/", "_")
-    //           .replace(/[^a-zA-Z0-9_\-\.]/g, "_");
-    //         return `vendor.${safeName}`;
-    //       } catch (e) {
-    //         return "vendor";
-    //       }
-    //     },
-    //   },
-    // },
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id || !id.includes("node_modules")) return;
+          try {
+            const parts = id.split("node_modules/")[1].split("/");
+            let pkg = parts[0];
+            if (pkg.startsWith("@") && parts.length > 1)
+              pkg = `${pkg}/${parts[1]}`;
+            const safeName = pkg
+              .replace("@", "")
+              .replace("/", "_")
+              .replace(/[^a-zA-Z0-9_\-\.]/g, "_");
+            return `vendor.${safeName}`;
+          } catch (e) {
+            return "vendor";
+          }
+        },
+      },
+    },
     outDir: "dist",
   },
 });
